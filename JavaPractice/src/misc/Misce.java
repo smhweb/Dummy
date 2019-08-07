a1b75510254d0d5f96e2f535048b284f09f3212a
mshafique <mshafique2022@gmail.com>
2019-08-07 14:24:53 -0400

    removed println

--- JavaPractice/src/misc/Misce.java
+++ JavaPractice/src/misc/Misce.java
@@ -9,8 +9,7 @@ public class Misce {
 		String a="Welcome to the interview sessions with Syntax";      
 		System.out.println(a.substring(39));      
 		String [] words=a.split(" ");   
-		System.out.println(words.length);   
-		System.out.println(words.length);   
+		System.out.println(words.length);  
 
 		   for (String string : words) {    
 			   System.out.println(string);   
